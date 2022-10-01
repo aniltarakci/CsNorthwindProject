52ed9b35801220728312e6c9a87d967d2a2caa78
ANIL <ANIL@DESKTOP-LQCPFUV>
2022-10-01 08:07:04 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            //AddProduct(productManager);
+            //UpdateProduct(productManager);
+            //DeleteProduct(productManager);
+            //ListProduct(productManager);
+            ListProductDetails(productManager);
+        }
+
+        private static void ListProductDetails(ProductManager productManager)
+        {
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+            }
+        }
+
+        private static void DeleteProduct(ProductManager productManager)
+        {
+            productManager.Delete(new Product { ProductId = 79 });
+        }
+
+        private static void UpdateProduct(ProductManager productManager)
+        {
+            productManager.Update(new Product { ProductId = 79, ProductName = "şemsiye", CategoryId = 3, UnitPrice = 150, UnitsInStock = 15 });
+        }
+
+        private static void AddProduct(ProductManager productManager)
+        {
+            productManager.Add(new Product { ProductName = "kalem", CategoryId = 2, UnitPrice = 100, UnitsInStock = 10 });
+        }
+
+        private static void ListProduct(ProductManager productManager)
+        {
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
